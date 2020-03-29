4731856ef4c18ba1e8b66ddb2c4157c7f9c1c5a7
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-03-30 05:26:00 +0530

    --- Auto-Git Commit ---

--- install.bat
+++ install.bat
@@ -1,8 +1,12 @@
 curl https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe -output python.exe
-python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0                             
 pip install jupyterlab
 pip install notebook
 pip install voila
 pip install jupyter_http_over_ws
 jupyter serverextension enable --py jupyter_http_over_ws
 jupyter notebook \ --NotebookApp.allow_origin='https://colab.research.google.com' \ --port=8888 \ --NotebookApp.port_retries=0
+
+
+
+REM python git requires cmd restart
