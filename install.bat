f7151a3f20df447ad76f61a4007a8c30fbb9f979
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-04-23 21:35:47 +0530

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-curl https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe -output python.exe
+curl https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe -o python.exe
 python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0                             
 pip install jupyterlab
 pip install notebook
